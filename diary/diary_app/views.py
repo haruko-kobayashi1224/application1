4aa92fe8b9d8c33ce3658077ddbf6e097ae9e7e0
Koba Hiro <h4330280@gmail.com>
2025-05-23 18:57:00 +0900

    登録画面のバリデーション追加

--- diary/diary_app/views.py
+++ diary/diary_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .forms import UserForm
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+
 def portfolio(request):
     return render(
         request, 'portfolio.html'              
@@ -12,6 +16,19 @@ def login(request):
     
 def regist(request):
     user_form = UserForm(request.POST or None)
+    if user_form.is_valid():
+        user = user_form.save(commit=False)
+        password =user_form.cleaned_data.get('password', '')
+        try:
+            validate_password(password)
+        except ValidationError as e:
+           user_form.add_error('password', e)
+           return render(request, 'user/registration.html', context={
+              'user_form': user_form,   
+           })
+        user.set_password(password)
+        user.seve()
+        
     return render(request, 'user/registration.html', context={
         'user_form': user_form,   
     })
