83545456431b13e642b732246914f9715d121889
Koba Hiro <h4330280@gmail.com>
2025-05-23 22:47:10 +0900

    バリデーションエラーの作成

--- diary/diary_app/forms.py
+++ diary/diary_app/forms.py
@@ -1,11 +1,33 @@
 from django import forms 
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 class UserForm(forms.ModelForm):
+    
+    confirm_password = forms.CharField(
+        label='パスワード再入力', widget=forms.PasswordInput()
+    )
     
     class Meta():
         model = User
         fields = ('username', 'email', 'password')
-        widgets = {
-            'password': forms.PasswordInput
+        labels = {
+            'username': '名前',
+            'email':'メールアドレス',
+            'password':'パスワード',
         }
+        widgets = {
+            'password': forms.PasswordInput()
+        }
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data['password']
+        confirm_password = cleaned_data['password']   
+        if password != confirm_password:
+            self.add_error('password', 'パスワードが一致しました')
+        try: 
+            validate_password(password, self.instance) 
+        except ValueError as e:
+            self.add_error('password', e)
+            return cleaned_data  
--- diary/diary_app/views.py
+++ diary/diary_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import UserForm
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.forms import UserCreationForm
 
 def portfolio(request):
     return render(
@@ -15,20 +15,22 @@ def login(request):
     )    
     
 def regist(request):
-    user_form = UserForm(request.POST or None)
-    if user_form.is_valid():
-        user = user_form.save(commit=False)
-        password =user_form.cleaned_data.get('password', '')
-        try:
-            validate_password(password)
-        except ValidationError as e:
-           user_form.add_error('password', e)
-           return render(request, 'user/registration.html', context={
-              'user_form': user_form,   
-           })
-        user.set_password(password)
-        user.save()
-        
+    # user_form = UserForm(request.POST or None)
+    # if user_form.is_valid():
+    #     user = user_form.save(commit=False)
+    #     password =user_form.cleaned_data.get('password', '')
+    #     try:
+    #         validate_password(password)
+    #     except ValidationError as e:
+    #        user_form.add_error('password', e)
+    #        return render(request, 'user/registration.html', context={
+    #           'user_form': user_form,   
+    #        })
+    #     user.set_password(password)
+    #     user.save()
+    user_form = UserCreationForm(request.POST or None)  
+    if user_form.is_valid(): 
+        user_form.save()
     return render(request, 'user/registration.html', context={
         'user_form': user_form,   
     })
